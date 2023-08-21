4fe4f09655076bd6690ba66f45b481015dd90885
fauza21104069 <129355292+fauza21104069@users.noreply.github.com>
2023-08-21 21:37:35 +0700

    TUGAS FAUZA IBRAHIM FARID
    coding php

---
+++ TUGAS_PHP_MSIBGITS/index.php
@@ -0,0 +1,6 @@
+<?php
+echo "Nama : Fauza Ibrahim Farid<br>";
+echo "Kampus : Institut Teknologi Telkom Purwokerto<br>";
+echo "Program Studi : Rekayasa Perangkat Lunak<br>";
+echo "Hobi : Motoran <br>";
+?>
