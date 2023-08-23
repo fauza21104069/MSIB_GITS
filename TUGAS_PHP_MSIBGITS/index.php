d1162a5e0a3d8053646bfb85cef19d6c7b251895
fauza21104069 <129355292+fauza21104069@users.noreply.github.com>
2023-08-23 19:14:43 +0700

    Update index.php
    HTML, CSS, PHP

--- TUGAS_PHP_MSIBGITS/index.php
+++ TUGAS_PHP_MSIBGITS/index.php
@@ -1,6 +1,20 @@
-<?php
-echo "Nama : Fauza Ibrahim Farid<br>";
-echo "Kampus : Institut Teknologi Telkom Purwokerto<br>";
-echo "Program Studi : Rekayasa Perangkat Lunak<br>";
-echo "Hobi : Motoran <br>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>BIODATA</title>
+</head>
+<body>
+    <h1>Biodata</h1>
+    <div>
+        <?php
+        echo "<center>Nama : Fauza Ibrahim Farid</center><br>";
+        echo "<center>Kampus : Institut Teknologi Telkom Purwokerto</center><br>";
+        echo "<center>Program Studi : Rekayasa Perangkat Lunak</center><br>";
+        echo "<center>Hobi : Motoran</center><br>";
+        ?>
+    </div>
+</body>
+</html>
